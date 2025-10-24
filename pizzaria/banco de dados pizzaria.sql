da818e7f0082e46508484b73105eb76bb210bc08
escritor <escritor759@gmail.com>
2025-10-24 16:38:46 -0300

    24.10.2025

---
+++ pizzaria/banco de dados pizzaria.sql
@@ -0,0 +1,23 @@
+create database pizzaria1;
+
+use exemplo;
+
+create table cliente1 (
+id_cliente int primary key
+);
+
+create table administrador1 (
+id_administrador int primary key
+);
+
+create table pizza1 (
+id_pizza int primary key
+);
+
+create table funcionario1 (
+id_funcionario int primary key
+);
+
+create table pedido1 (
+id_pedido int primary key
+);
